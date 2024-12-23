1b8e897ace4a0b2ef139b31d996110a9ae8bb9ee
JustPowerful <ahmedaminedoudech@gmail.com>
2024-12-23 14:46:46 +0100

    feat: add reduceStr utility function for string truncation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+// reduce string with n characters and add '...' at the end
+export function reduceStr(str: string, n: number) {
+  return str.length > n ? str.slice(0, n) + "..." : str;
 }
