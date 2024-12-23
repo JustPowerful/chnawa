8d31d84b0f533be484a31f0dac975057ef41b1a1
JustPowerful <ahmedaminedoudech@gmail.com>
2024-12-23 22:07:29 +0100

    refactor: comment out MONGODB_URI error handling for flexibility in environment configuration

--- src/lib/connectDB.ts
+++ src/lib/connectDB.ts
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.DATABASE_URL!;
 
-if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable");
-}
+// if (MONGODB_URI.length === 0) {
+//   throw new Error("Please define the MONGODB_URI environment variable");
+// }
 
 let isConnected = false;
 
