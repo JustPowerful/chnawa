6245cf73b6a63107288251c247c9e299298961b8
JustPowerful <ahmedaminedoudech@gmail.com>
2025-01-15 22:52:54 +0100

    disable eslint for build to speed up compilation

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint during builds to speed up compilation
+  },
 };
 
 export default nextConfig;
