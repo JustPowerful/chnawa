dfdbae2f846d2e50dfa918735a67444170357641
JustPowerful <ahmedaminedoudech@gmail.com>
2025-01-15 22:58:20 +0100

    fix: add configuration for mongodb on edge

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,8 @@
 export { auth as middleware } from "@/lib/auth";
+export const config = {
+  matcher: "/:path*",
+  unstable_allowDynamic: [
+    "/src/lib/connectDB.ts",
+    "/node_modules/mongoose/dist/**",
+  ],
+};
